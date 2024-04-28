1c312b03c988ac2f376aa6c2d02eefa468c56028
Arnav Varshney <varshney.arnav@gmail.com>
2024-04-28 21:06:55 +0800

    Add error-handling for empty evaluation.json

--- server.py
+++ server.py
@@ -9,73 +9,40 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    [
-        html.H1("ELEC3442 Chess Bot Analysis", style={"textAlign": "center"}),
-        html.H6("Auto-updated every 3 seconds", style={"textAlign": "center"}),
-        dcc.Graph(id="cp-chart"),
-        dash_table.DataTable(
-            id="move-table",
-            columns=[
-                {"name": "Color", "id": "id"},
-                {"name": "Move Count", "id": "move_count"},
-                {"name": "Your Move", "id": "move"},
-                {"name": "Best Move", "id": "best_move"},
-                {"name": "Time Left (s)", "id": "time_left"},
-                {"name": "Centipawn", "id": "cp"},
-            ],
-            style_cell={"textAlign": "center"},
-            style_header={
-                "backgroundColor": "rgb(230, 230, 230)",
-                "fontWeight": "bold",
-            },
-        ),
-        dcc.Interval(
-            id='interval-component',
-            interval=3 * 1000,  # in milliseconds
-            n_intervals=0
-        ),
-    ], style={"width": "80%", "margin": "auto", "font-family": "Comic Sans MS"}
-)
+app.layout = html.Div([html.H1("ELEC3442 Chess Bot Analysis", style={"textAlign": "center"}),
+    html.H6("Auto-updated every 3 seconds", style={"textAlign": "center"}), dcc.Graph(id="cp-chart"),
+    dash_table.DataTable(id="move-table",
+        columns=[{"name": "Color", "id": "id"}, {"name": "Move Count", "id": "move_count"},
+            {"name": "Your Move", "id": "move"}, {"name": "Best Move", "id": "best_move"},
+            {"name": "Time Left (s)", "id": "time_left"}, {"name": "Centipawn", "id": "cp"}, ],
+        style_cell={"textAlign": "center"},
+        style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold", }, ),
+    dcc.Interval(id='interval-component', interval=3 * 1000,  # in milliseconds
+        n_intervals=0), ], style={"width": "80%", "margin": "auto", "font-family": "Comic Sans MS"})
 
 
-@app.callback(
-    Output('cp-chart', 'figure'),
-    Output('move-table', 'data'),
-    Input('interval-component', 'n_intervals')
-)
+@app.callback(Output('cp-chart', 'figure'), Output('move-table', 'data'), Input('interval-component', 'n_intervals'))
 def update_layout(n):
-    with open("evaluation.json", "r") as f:
-        data = json.load(f)
-        print(data)
+    try:
+        with open("evaluation.json", "r") as f:
+            data = json.load(f)
+            print(data)
 
-    df = pd.DataFrame(data)
+        df = pd.DataFrame(data)
 
-    figure = {
-        "data": [
-            {
-                "x": df[df["id"] == "black"]["move_count"],
-                "y": df[df["id"] == "black"]["cp"],
-                "type": "line",
-                "name": "Black",
-            },
-            {
-                "x": df[df["id"] == "white"]["move_count"],
-                "y": df[df["id"] == "white"]["cp"],
-                "type": "line",
-                "name": "White",
-            },
-        ],
-        "layout": {
-            "title": "Centipawn Plot",
-            "xaxis_title": "Move Count",
-            "yaxis_title": "Centipawn",
-        },
-    }
+        figure = {"data": [
+            {"x": df[df["id"] == "black"]["move_count"], "y": df[df["id"] == "black"]["cp"], "type": "line",
+                "name": "Black", },
+            {"x": df[df["id"] == "white"]["move_count"], "y": df[df["id"] == "white"]["cp"], "type": "line",
+                "name": "White", }, ],
+            "layout": {"title": "Centipawn Plot", "xaxis_title": "Move Count", "yaxis_title": "Centipawn", }, }
 
-    data = df.to_dict("records")
+        data = df.to_dict("records")
 
-    return figure, data
+        return figure, data
+
+    except (json.JSONDecodeError, KeyError):
+        return {}, []
 
 
 if __name__ == "__main__":
